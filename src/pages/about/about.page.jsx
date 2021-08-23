3f22034df4794961720d7d0678d1280412db21ff
tccSolutions <tim@tccs.tech>
2021-08-23 12:30:16 -0400

    updated about page

--- src/pages/about/about.page.jsx
+++ src/pages/about/about.page.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import {Navbar} from '../../components/navbar/navbar.component'
+import "./about.styles.scss"
 
 export const AboutPage = () => (
-    <div>
+    <div className='about'>
         <Navbar/>
       
-        <h1>UNDER CONSTRUCTION</h1>
+        <h1>About Us</h1>
+        <h3 className='message'>Tim began repairing and building computers when he was 12 years old. He soon discovered that he had a passion
+            for computers and he started to learn to code. As the years went on he obtained his Bachelor's Degree in Computer Information Systems.
+            He has since devoloped programs and websites for many different people and companies. Check out the <a href='/work'>OUR WORK</a> page to
+            see a few examples. If you have any questions or would like to contact us for any reason, please use the form found on the
+            <a href='/contactus'> Contact Us</a> page.
+        </h3>
        
        
     </div>
